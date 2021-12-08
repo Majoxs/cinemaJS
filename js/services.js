aa5c1fbd74b9c4832d74abe8f008b2f63f91fde1
Majoxs <majoxs@gmail.com>
2021-12-08 16:14:57 +0500

    FEAT: Added new functional

---
+++ js/services.js
@@ -0,0 +1,22 @@
+const API_KEY = '5a2e12de7305619e9353d547e7f726e8';
+const BASE_URL = 'https://api.themoviedb.org/3/';
+const LANGUAGE = '&language=ru-RU';
+
+//trending/all/day?api_key=<<api_key>>
+
+const getData = url => fetch(url)
+  .then(response => {
+    if (response.ok) {
+      return response.json()
+    }
+    throw `Что-то пошло не так, ошибка: ${response.status}`
+  })
+  .catch(err => console.error(err));
+
+
+
+export const getTriends = async (type = 'all', period = 'day', page = 1) => {
+  const url = `${BASE_URL}trending/${type}/${period}?api_key=${API_KEY}${LANGUAGE}&page=${page}`;
+  return await getData(url);
+};
+
